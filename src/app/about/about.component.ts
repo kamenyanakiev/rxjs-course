a98a4009c86b4c7c78b9057130ad838c44c7a54d
kamen-yanakiev <kamenqnakiev@gmail.com>
2020-12-22 17:07:19 +0200

    base project set up, first part done

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,53 @@
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+    concat,
+    fromEvent,
+    interval,
+    noop,
+    observable,
+    Observable,
+    of,
+    timer,
+    merge,
+    Subject,
+    BehaviorSubject,
+    AsyncSubject,
+    ReplaySubject
+} from 'rxjs';
+import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
+import {createHttpObservable} from '../common/util';
+
+
+@Component({
+    selector: 'about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+    ngOnInit() {
+
+        const http$ = new Observable(observer => {
+            fetch('/api/courses')
+                .then(res => {
+                    return res.json();
+                })
+                .then(body => {
+                    observer.next(body);
+                    observer.complete();
+                })
+                .catch(err => {
+                    observer.error(err);
+                });
+
+        });
+
+        http$.subscribe(
+            courses => console.log(courses),
+            noop,
+            () => console.log('completed')
+        );
+
+    }
+
+}
